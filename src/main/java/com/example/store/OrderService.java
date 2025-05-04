dff154a8b76d6f60cda80c102b0ca569e58b0f2c
amal <95304547+amal480@users.noreply.github.com>
2025-05-04 13:07:29 +0530

    IOC Container

--- src/main/java/com/example/store/OrderService.java
+++ src/main/java/com/example/store/OrderService.java
@@ -1,16 +1,26 @@
 package com.example.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
+    public OrderService(PaymentService paymentService,int x){
+
+    }
+
     //    we receive a payment service object here in this constructor
-//    public OrderService(PaymentService paymentService){
-//        this.paymentService=paymentService;
-//    }
+    @Autowired
+    public OrderService(PaymentService paymentService){
+        this.paymentService=paymentService;
+    }
     public void placeOrder(){
         paymentService.processPayment(10);
     }
-    public void setPaymentService(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+//    public void setPaymentService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 }
