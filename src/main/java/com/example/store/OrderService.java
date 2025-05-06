34b3d0638c2b6fc13ffb490aa3a1c3fc3c41d177
amal <95304547+amal480@users.noreply.github.com>
2025-05-06 14:53:23 +0530

    Bean lifecycle hooks

--- src/main/java/com/example/store/OrderService.java
+++ src/main/java/com/example/store/OrderService.java
@@ -1,5 +1,7 @@
 package com.example.store;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -19,6 +21,16 @@ public class OrderService {
         this.paymentService=paymentService;
         System.out.println("OrderService created");
     }
+    @PostConstruct
+    public void init(){
+        System.out.println("OrderService PostConstruct");
+    }
+    @PreDestroy
+    public void cleanup(){
+        System.out.println("Pre Destroy");
+
+    }
+
     public void placeOrder(){
         paymentService.processPayment(10);
     }
