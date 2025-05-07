0905a2fb87c1d5c4dae83862ae147254511cba34
amal <95304547+amal480@users.noreply.github.com>
2025-05-07 22:42:42 +0530

    LOMBOK

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.example.store;
 
+import com.example.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,8 +11,12 @@ public class StoreApplication {
 
 	public static void main(String[] args) {
 
-		ApplicationContext context=SpringApplication.run(StoreApplication.class, args);
-
+//		ApplicationContext context=SpringApplication.run(StoreApplication.class, args);
+		var user=User.builder()
+				.name("john")
+				.password("password")
+				.email("example.mail")
+				.build();
 	}
 
 }
--- src/main/java/com/example/store/entities/User.java
+++ src/main/java/com/example/store/entities/User.java
@@ -1,7 +1,13 @@
 package com.example.store.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name="users")
 public class User {
@@ -17,37 +23,5 @@ public class User {
 
     @Column(nullable=false,name="password")
     private String password;
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
 }
