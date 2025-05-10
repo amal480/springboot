5e77c372bd2e55e56fc3954d671101b03758f816
amal <95304547+amal480@users.noreply.github.com>
2025-05-10 15:57:06 +0530

    eager and lazy loading

--- src/main/java/com/example/store/services/UserService.java
+++ src/main/java/com/example/store/services/UserService.java
@@ -1,6 +1,8 @@
 package com.example.store.services;
 
 import com.example.store.entities.User;
+import com.example.store.repositories.AddressRepository;
+import com.example.store.repositories.ProfileRepository;
 import com.example.store.repositories.UserRepository;
 import jakarta.persistence.EntityManager;
 import lombok.AllArgsConstructor;
@@ -12,8 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 public class UserService {
     private final EntityManager entityManager;
-
+    private final ProfileRepository profileRepository;
     private final UserRepository userRepository;
+    private final AddressRepository addressRepository;
 
     @Transactional
     public void showEntityStates(){
@@ -35,5 +38,15 @@ public class UserService {
         else{
             System.out.println("Transient or detached");
         }
+    }
+
+    @Transactional
+    public void showRelatedEntities(){
+        var profile=profileRepository.findById(4L).orElseThrow();
+        System.out.println(profile.getUser().getEmail());
+    }
+
+    public void fetchAddress(){
+        var address=addressRepository.findById(1L).orElseThrow();
     }
 }
