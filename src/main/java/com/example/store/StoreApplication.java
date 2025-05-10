63464f91c501c4b00533454409bdc304646c8da7
amal <95304547+amal480@users.noreply.github.com>
2025-05-10 13:35:19 +0530

    Transaction

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -1,5 +1,8 @@
 package com.example.store;
 
+import com.example.store.entities.User;
+import com.example.store.repositories.UserRepository;
+import com.example.store.services.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,17 +13,9 @@ public class StoreApplication {
     public static void main(String[] args) {
 
 		ApplicationContext context=SpringApplication.run(StoreApplication.class, args);
-//        var user = User.builder()
-//                .name("john")
-//                .password("password")
-//                .email("example.mail")
-//                .build();
-//        var profile= Profile.builder()
-//                        .bio("bio")
-//                                .build();
-//        user.setProfile(profile);
-//        profile.setUser(user);
-//        System.out.println(user);
+        var repository=context.getBean(UserRepository.class);
+        var userService=context.getBean(UserService.class);
+        userService.showEntityStates();
     }
 
 }
---
+++ src/main/java/com/example/store/services/UserService.java
@@ -0,0 +1,39 @@
+package com.example.store.services;
+
+import com.example.store.entities.User;
+import com.example.store.repositories.UserRepository;
+import jakarta.persistence.EntityManager;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final EntityManager entityManager;
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void showEntityStates(){
+        var user= User.builder()
+                .name("John")
+                .email("johnmail@mail.com")
+                .password("password")
+                .build();
+        if (entityManager.contains(user)){
+            System.out.println("Persistent");
+        }
+        else{
+            System.out.println("Transient or detached");
+        }
+    userRepository.save(user);
+        if (entityManager.contains(user)){
+            System.out.println("Persistent");
+        }
+        else{
+            System.out.println("Transient or detached");
+        }
+    }
+}
